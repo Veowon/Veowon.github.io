c32768e1df6773c760eea75ba87618d1a2f56316
Veowon <mega.andrey.345@gmail.com>
2023-08-06 02:14:08 +0400

    Updated main title

---
+++ assets/components/content/main.php
@@ -0,0 +1,365 @@
+<main>
+	<section>
+		<div class="container-fluid px-0">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="main_img d-flex flex-column justify-content-center">
+						<div class="main_img_title text-center">
+							<h1>Говорят, никогда не поздно<br> сменить профессию</h1>
+							<p>Сделай круто тестовое задание и у тебя получится</p>
+						</div>
+						<div class="main_img_button text-center mt-5">
+							<button><h5>Проще простого!</h5></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="benefits">
+		<div class="container">
+			<div class="row mt-5 mb-3">
+				<div class="col-xl-12">
+					<div>
+						<div class="benefits_title text-center mb-5">
+							<h2>Как это работает</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row mb-5">
+				<div class="col-xl-3 col-md-6">
+					<div class="benefits_item align-items-md-start d-flex d-xl-block">
+						<img src="assets/img/benefits/waiting.png" class="mb-3 mr-3">
+						<div>
+							<h6>Прочитай задание внимательно</h6>
+							<p>Думаю у тебя не займет это больше двух-трех минут</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-md-6">
+					<div class="benefits_item align-items-md-start d-flex d-xl-block">
+						<img src="assets/img/benefits/delivery-truck.png" class="mb-3 mr-3">
+						<div>
+							<h6>Изучи весь макет заранее</h6>
+							<p>Подумай как это будет работать на разных разрешениях и при скролле</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-md-6">
+					<div class="benefits_item align-items-md-start d-flex d-xl-block">
+						<img src="assets/img/benefits/secure.png" class="mb-3 mr-3">
+						<div>
+							<h6>Сделай хорошо</h6>
+							<p>Чтобы мы могли тебе доверить подобные задачи в будущем</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-3 col-md-6">
+					<div class="benefits_item align-items-md-start d-flex d-xl-block">
+						<img src="assets/img/benefits/money-bags.png" class="mb-3 mr-3">
+						<div>
+							<h6>Получи предложение</h6>
+							<p>Ну тут все просто, не я придумал правила, но думаю так и будет)))</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row align-items-center mb-5">
+				<div class="col-xl-6 col-md-6 order-md-1 order-2">
+					<div class="benefits_infobox_text text-center text-sm-left">
+						<h3>Круто, ты дошел до третьего блока</h3>
+						<p>63% опрошенных пользовались кредитами из-за того, что не могли покрыть непредвиденные расходы свыше 15 000 ₽.</p>
+						<p>Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах.</p>
+					</div>
+				</div>
+				<div class="col-xl-6 col-md-6 order-md-2 order-1 mb-sm-0 mb-5">
+					<div class="benefits_img_wrapper">
+						<img class="benefits_img_1" src="assets/img/benefits/2_2.png">
+						<img class="benefits_img_2 position-absolute" src="assets/img/benefits/2_1.png">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="reviews">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<div>
+						<div class="benefits_title text-center my-5">
+							<h2>Отзывы</h2>
+						</div>
+					</div>
+					<div id="carouselReviews" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators z-3">
+						    <li data-target="#carouselReviews" data-slide-to="0" class="active"></li>
+						    <li data-target="#carouselReviews" data-slide-to="1"></li>
+							<li data-target="#carouselReviews" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+
+						    <div class="carousel-item active">
+						    	<div class="reviews_items justify-content-around d-none d-lg-flex d-xl-flex">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_2.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Иван Иванов</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы упускаем, что активно развивающиеся страны третьего мира призваны к ответу.</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_3.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Артем Корнилов</h6>
+						    					<p>Самара</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения.</p>
+						    			</div>
+						    		</div>
+						    	</div>
+
+						    	<div class="reviews_items justify-content-around d-none d-lg-none d-xl-none d-md-flex d-sm-flex">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_2.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Иван Иванов</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы упускаем, что активно развивающиеся страны третьего мира призваны к ответу.</p>
+						    			</div>
+						    		</div>
+						    	</div>
+
+						    	<div class="reviews_items justify-content-around d-flex d-md-none d-sm-none">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    	</div>
+						    </div>
+
+						    <div class="carousel-item">
+						    	<div class="reviews_items justify-content-around d-none d-lg-flex d-xl-flex">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_2.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Иван Иванов</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы упускаем, что активно развивающиеся страны третьего мира призваны к ответу.</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_3.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Артем Корнилов</h6>
+						    					<p>Самара</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения.</p>
+						    			</div>
+						    		</div>
+						    	</div>
+
+						    	<div class="reviews_items justify-content-around d-none d-lg-none d-xl-none d-md-flex d-sm-flex">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_2.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Иван Иванов</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы упускаем, что активно развивающиеся страны третьего мира призваны к ответу.</p>
+						    			</div>
+						    		</div>
+						    	</div>
+
+						    	<div class="reviews_items justify-content-around d-flex d-md-none d-sm-none">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    	</div>
+						    </div>
+
+						    <div class="carousel-item">
+						    	<div class="reviews_items justify-content-around d-none d-lg-flex d-xl-flex">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_2.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Иван Иванов</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы упускаем, что активно развивающиеся страны третьего мира призваны к ответу.</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_3.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Артем Корнилов</h6>
+						    					<p>Самара</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения.</p>
+						    			</div>
+						    		</div>
+						    	</div>
+
+						    	<div class="reviews_items justify-content-around d-none d-lg-none d-xl-none d-md-flex d-sm-flex">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_2.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Иван Иванов</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы упускаем, что активно развивающиеся страны третьего мира призваны к ответу.</p>
+						    			</div>
+						    		</div>
+						    	</div>
+
+						    	<div class="reviews_items justify-content-around d-flex d-md-none d-sm-none">
+						    		<div class="reviews_item p-3">
+						    			<div class="d-flex align-items-start">
+						    				<img src="assets/img/reviews/Ellipse_1.png">
+						    				<div class="reviews_item_name ml-3">
+						    					<h6>Константинов Михаил Павлович</h6>
+						    					<p>Санкт-Петербург</p>
+						    				</div>
+						    			</div>
+						    			<div class="reviews_item_description">
+						    				<p>Каждый из нас понимает очевидную вещь: перспективное планирование предоставляет широкие возможности для анализа существующих паттернов поведения. В своём стремлении улучшить пользовательский опыт мы</p>
+						    			</div>
+						    		</div>
+						    	</div>
+						    </div>
+
+						</div>
+						<a class="carousel-control-prev d-none d-lg-flex" href="#carouselReviews" role="button" data-slide="prev">
+						    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next d-none d-lg-flex" href="#carouselReviews" role="button" data-slide="next">
+						    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+						    <span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	include 'assets/components/header.php';
+	include 'assets/components/content/main.php';
+	include 'assets/components/footer.php';
+?>
